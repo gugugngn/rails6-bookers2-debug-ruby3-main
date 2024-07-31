3982f30320899816faf13426d683311f47c35ebf
gugugngn <173164252+gugugngn@users.noreply.github.com>
2024-07-31 05:18:06 +0000

    課題６　途中

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)  # 現在ログインしているユーザの新規コメントを”comment"とする
-    comment.book_id = book.id  # コメントが投稿された特定の本に"comment.book_id"と名前つけ
+    comment.book_id = @book.id  # コメントが投稿された特定の本に"comment.book_id"と名前つけ
     comment.save  # ようやく保存
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   
   def destroy 
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    # redirect_to request.referer
   end
   
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,18 @@
 class FavoritesController < ApplicationController
   def create
+    @book = Book.find(params[:book_id])
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)   # 現在ログインしているユーザーが特定の本に「いいね」するための行為を「favorite」という変数に定義
                                                               # book = Book.find(params[:book_id])ここで見つけてきた特定の本のレコード、book_idをbook.idに代入している
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 end
